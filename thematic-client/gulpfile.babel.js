2a650b310d7eb98ccf06de3148d79a1ce78df830
José Maria Villac Pinheiro <jmpinheiro@nexusbr.com>
2015-09-11 16:10:24 -0300

    Realizada a segunda sincronização de testes

--- thematic-client/gulpfile.babel.js
+++ thematic-client/gulpfile.babel.js
@@ -1,5 +1,5 @@
 // generated on 2015-06-18 using generator-gulp-webapp 1.0.0
-// testando sincronização
+// segunda sincronização
 import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
 import browserSync from 'browser-sync';
