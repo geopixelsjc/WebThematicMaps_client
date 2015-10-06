9f224de5605efaf1b2b095cef479acf2f1d3127f
David <dvd.pansardis@gmail.com>
2015-10-06 14:06:30 -0300

    Finished the modal form client

--- thematic-client/app/scripts/ThematicMap.js
+++ thematic-client/app/scripts/ThematicMap.js
@@ -11,6 +11,7 @@ var ThematicMapModule = function() {
             self = this;
             self.initializeModallisteners();
             self.initializeButtonsListeners();
+            self.initializeRadiolisteners();
             self.initializeTextlisteners();
             self.initializeComboBoxlisteners();
         },
@@ -24,29 +25,12 @@ var ThematicMapModule = function() {
         initializeButtonsListeners: function() {
             $('#showModal').click(function(){
                 $("#thematicModal").modal("show");
-                /*
+
                 var url = "http://localhost:8080/WebThematicMaps_server/rest/json/indicators?";
-                url += "table=" + "tab_indicadores";
+                url += "table="+"tab_indicadores";
                 $.getJSON( url, function( data ) {
-                    var optAttributes="";
-                    var optYears="";
-                    //for (var i = data.length - 1; i >= 0; i--) {
-                        
-                    //};
-                    $("#attribute-selection").html(optAttributes);
-                });
-                */
-                var url = "http://localhost:8080/WebThematicMaps_server/rest/json/indicators?";
-                url += "table=" + "tab_valores";
-                $.getJSON( url, function( data ) {
-                    var optAttributes="";
-                    var optYears="";
-                    //for (var i = data.length - 1; i >= 0; i--) {
-                        optAttributes += '<option value="' + data.id + '">' + data.nome + '</option>';
-                        optYears += '<option value="' + data.id + '">' + data.ano + '</option>';
-                    //};
-                    $("#attribute-selection").html(optAttributes);
-                    $("#year-selection").html(optYears);
+                    self.tableIndicators = data;
+                    
                 });
             });
             $("#executeThematicButton").click(function() {
@@ -55,34 +39,46 @@ var ThematicMapModule = function() {
                     validate = false;
                     alert("Entre com o nome para o novo tema.");
                     $("#name-thematic").focus();
+                    return;
                 }else{
                     if(Map.existsLayer($("#name-thematic").val())){
                         validate = false;
                         alert("Este nome já existe.");
                         $("#name-thematic").focus();
+                        return;
                     }
+                }
+                if(!$("#optC").is(":checked") && !$("#optA").is(":checked")  && !$("#optI").is(":checked") ){
+                    validate = false;
+                    alert("Selecione o tipo de pesquisa.");
+                    return;
+                }
+                if($("#year-selection").val().length == 0){
+                    validate = false;
+                    alert("Selecione o ano.");
+                    $("#year-selection").focus();
+                    return;
                 }
                 if($("#classes-number").val().length == 0){
                     validate = false;
                     alert("Entre com a quantidade de classes.");
                     $("#classes-number").focus();
+                    return;
                 }
                 if($("#first-color").val().length == 0){
                     validate = false;
                     alert("Selecione a cor inicial.");
                     $("#first-color").focus();
+                    return;
                 }
                 if($("#last-color").val().length == 0){
                     validate = false;
                     alert("Selecione a cor final.");
                     $("#last-color").focus();
+                    return;
                 }
                 if(validate){
-                    self.nameThematic = $("#name-thematic").val();
-                    self.classesNumber = $("#classes-number").val();
-                    self.firstColor = String($("#first-color").val());
-                    self.lastColor = String($("#last-color").val());
-
+                    self.thematicName = $("#name-thematic").val();
                     self.generateGeoJSON();
 
                     var thematicModal = $('#thematicModal');
@@ -94,71 +90,50 @@ var ThematicMapModule = function() {
             });
         },
 
+        initializeRadiolisteners: function(){
+            $("#optC").click(function(){
+                self.loadComboAttributes("C");
+            });
+            $("#optA").click(function(){
+                self.loadComboAttributes("A");
+            });
+            $("#optI").click(function(){
+                self.loadComboAttributes("I");
+            });
+        },
+
+        loadComboAttributes: function(opt){
+            var optAttributes="";
+            for (var i = self.tableIndicators.length - 1; i >= 0; i--) {
+                if(self.tableIndicators[i].indicador[0] == opt){
+                    optAttributes += '<option value="' + self.tableIndicators[i].id + '">' + self.tableIndicators[i].nome + '</option>';
+                }
+            };
+            $("#attribute-selection").html(optAttributes);
+            self.loadDescription();
+        },
+
         initializeTextlisteners: function(){
        
         },
 
         initializeComboBoxlisteners : function(){
-            /*
-            $("#attribute-selection").click(function(){
-                var url = "";
-                $.getJSON( url, function( data ) {
-                    var options="";
-                    $.each(data.opts, function (key, val) {
-                        options += '<option value="' + val + '">' + val + '</option>';
-                    });
-                    $("#attribute-selection").html(options);
-                };
-            });
-            */
             $("#attribute-selection").change(function(){
-                alert($("#attribute-selection").val());
+                self.loadDescription();
             });
-            /*
-            $("#year-selection").click(function(){
-                var url = "";
-                $.getJSON( url, function( data ) {
-                    var options="";
-                    $.each(data.opts, function (key, val) {
-                        options += '<option value="' + val + '">' + val + '</option>';
-                    });
-                    $("#attribute-selection").html(options);
-                };
-            });
-            */
-            $("#year-selection").change(function(){
-                alert($("#year-selection").val());
-            });
-            /*
-            $("#type-selection").click(function(){
-                var url = "";
-                $.getJSON( url, function( data ) {
-                    var options="";
-                    $.each(data.opts, function (key, val) {
-                        options += '<option value="' + val + '">' + val + '</option>';
-                    });
-                    $("#attribute-selection").html(options);
-                };
-            });
-            */
-            $("#type-selection").change(function(){
-                alert($("#year-selection").val());
-            });
-            /*
-            $("#type-selection").click(function(){
-                var url = "";
-                $.getJSON( url, function( data ) {
-                    var options="";
-                    $.each(data.opts, function (key, val) {
-                        options += '<option value="' + val + '">' + val + '</option>';
-                    });
-                    $("#attribute-selection").html(options);
-                };
-            });
-            */
-            $("#type-selection").change(function(){
-                alert($("#year-selection").val());
-            });
+        },
+
+        loadDescription: function(){
+            var attribute = self.loadRowAttributeById($("#attribute-selection").val());
+            $("#description-font").val(attribute.descricao);
+        },
+
+        loadRowAttributeById: function(id){
+            for (var i = self.tableIndicators.length - 1; i >= 0; i--) {
+                if(self.tableIndicators[i]["id"] == id){
+                    return self.tableIndicators[i];
+                }
+            };
         },
         
         clearForm: function(){
@@ -166,11 +141,23 @@ var ThematicMapModule = function() {
             $("#classes-number").val("");
             $("#first-color").val("");
             $("#last-color").val("");
-            //$("#first-color").removeClass("color").addClass("color");
-            //$("#last-color").removeClass("color").addClass("color");
+            $("#optC").prop('checked', false);
+            $("#optA").prop('checked', false);
+            $("#optI").prop('checked', false);
+            $("#year-selection").val("");
+            $("#description-font").val("");
+            $("#attribute-selection").empty();
+            $("#type-selection").val("Quantil");
         },
 
         generateGeoJSON: function() {
+            var typeSelecion = "";
+            if($("#type-selection").val() == "Quantil"){
+                typeSelecion = "quantiles";
+            } else {
+                typeSelecion = "steps";
+            }
+
             var url = "http://localhost:8080/WebThematicMaps_server/rest/json/choroplethmap?";
             
             url += "table=" + "tab_valores";
@@ -181,38 +168,43 @@ var ThematicMapModule = function() {
             url += "&featurecode=" + "cod_ibge";
             url += "&featurename=" + "municipio";
             url += "&box=" + "x";
-            url += "&groupingtype=" + "quantiles";
-            url += "&nclasses=" + self.classesNumber;
-            url += "&firstcolor=" + self.firstColor.substring(1, self.firstColor.length);
-            url += "&lastcolor=" + self.lastColor.substring(1, self.lastColor.length);;
+            url += "&groupingtype=" + typeSelecion;
+            url += "&nclasses=" + $("#classes-number").val();
+            url += "&firstcolor=" + String($("#first-color").val()).substring(1, String($("#first-color").val()).length);
+            url += "&lastcolor=" + String($("#last-color").val()).substring(1, String($("#last-color").val()).length);;
             url += "&year=" + "ano";
-            url += "&targetyear=" + "2013";
-            url += "&targetattribute=" + "abamectina";
+            url += "&targetyear=" + $("#year-selection option:selected").text();
+            url += "&targetattribute=" +$("#attribute-selection option:selected").text();
 
 
             $.getJSON( url, function( data ) {
-                var formaterJson = new OpenLayers.Format.GeoJSON()
+                if(data.map != null){
+                    var formaterJson = new OpenLayers.Format.GeoJSON()
 
-                var featuresFromJson = formaterJson.read(
-                    data.map
-                );
-                
-                var vectors = new OpenLayers.Layer.Vector(self.nameThematic, {
-                    styleMap : new OpenLayers.StyleMap({
-                        "default": new OpenLayers.Style({
-                            strokeColor: "#000000",
-                            strokeOpacity: .7,
-                            strokeWidth: 1,
-                            fillColor: "${color}",
-                            fillOpacity: 0.8,
-                            cursor: "pointer"
+                    var featuresFromJson = formaterJson.read(
+                        data.map
+                    );
+                    
+                    var vectors = new OpenLayers.Layer.Vector(self.thematicName, {
+                        styleMap : new OpenLayers.StyleMap({
+                            "default": new OpenLayers.Style({
+                                strokeColor: "#000000",
+                                strokeOpacity: .7,
+                                strokeWidth: 1,
+                                fillColor: "${color}",
+                                fillOpacity: 0.8,
+                                cursor: "pointer"
+                            })
                         })
-                    })
-                });
-                
-                vectors.addFeatures(featuresFromJson);
-                
-                Map.addGeoJSONFeatures(vectors);
+                    });
+                    
+                    vectors.addFeatures(featuresFromJson);
+                    
+                    Map.addGeoJSONFeatures(vectors);    
+                } else {
+                    alert("Não retornou nenhum dado deste filtro");
+                    return;
+                }
             });
             
         }
